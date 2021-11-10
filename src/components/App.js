7b0e4d31ba159a486e158b2d0f0f2d3a70f3ed21
kaye garcia <kayemgarcia@gmail.com>
2021-11-10 08:05:19 -0600

    commit tree b2276c34503ccf26d9dcd34508bf1d5fd3f9be98

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushiData, setSushiData] = useState([]);
+  const [wallet, setWallet] = useState (80);
+
+  useEffect(() => {
+    fetch(API)
+      .then((r) => r.json())
+      .then((sushiData) => {
+        const updatedSushis = sushiData.map((sushi) => {
+          return {...sushi, eaten: false};
+        });
+        setSushiData(updatedSushis)
+      });
+    }, []);
+    
+  function handleEatenSushi(eatenSushi) {
+    if (wallet >= eatenSushi.price) {
+      const updatedSushis = sushiData.map((sushi) => {
+        if (sushi.id === eatenSushi.id) return {...sushi, eaten: true}
+          return sushi;
+      });
+
+      setSushiData(updatedSushis);
+      setWallet((wallet) => wallet - eatenSushi.price);
+    } else {
+      alert("You don't have any money left!");
+    }
+  }  
+
+  const eatenSushis = sushiData.filter((sushi) => sushi.eaten)
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer 
+        sushi={sushiData}
+        onEatSushi={handleEatenSushi}
+      />
+      <Table wallet={wallet} plate={eatenSushis} />
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi"
 
-function SushiContainer(props) {
+function SushiContainer({ sushi, onEatSushi }) {
+  const [visibleSushi, setVisibleSushi] = useState(0);
+  
+  const sushiCards = sushi
+    .slice(visibleSushi, visibleSushi + 4)
+    .map((sushi) => (
+      <Sushi key={sushi.id} sushi={sushi} onEatSushi={onEatSushi}/>
+  ));
+  
+  function handleLoadMore(){
+    setVisibleSushi((visibleSushi) => (visibleSushi + 4));
+  }
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiCards}
+      <MoreButton onClickMore={handleLoadMore}/>
     </div>
   );
 }
